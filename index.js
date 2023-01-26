eced02d49e366ea332146b5895e127e17d62682f
Vansh=Narang <vanshnarang49906@gmail.com>
2023-01-26 16:55:23 +0530

    Sum

--- index.js
+++ index.js
@@ -4,26 +4,36 @@ let hasBlack = false
 let isAlive = true
 let sum = firstcard + secondcard
 let message = ""
+// let sumEL = document.getElementById('Sumel')
 //Black jack game if equal to 21
 //If more than 21 you lose
 //And less than 21 you are nearly correct
+let messageEL = document.getElementById('message-el')
+
+//Use query selector instead of getELementById
+// let sumEL = document.querySelector('#Sumel')//Using when sumel is a id
+let sumEL = document.querySelector('.Sumel')//Using when sumel is class
 
 //If else conditions
-if (sum <= 20) {
-    message = "Do you want to draw a new card 😃"
-}
-else if (sum === 21) {
-    hasBlack = true
+function StartGame() {
+    sumEL.textContent = "Sum :" + " " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card "
+    }
+    else if (sum === 21) {
+        hasBlack = true
 
-    message = "You won 🎉"
+        message = "You won "
+    }
+    else {
+        isAlive = false
+        message = "You lose "
+    }
+    messageEL.textContent = "Want to draw a Card ?"
 }
-else {
-    isAlive = false
-    message = "You lose 😭"
-}
-
 //Cash out of money
 //Whether you lose or win then we will track using a variable
 //Boolean variable
+
 
 
